9cec9d6b9718c1f934cf4260520ffbc22ca97b3b
matthijsberg <matthijs@b3rg.nl>
2019-05-23 22:53:39 +0200

    Update unipipython.py

--- unipipython.py
+++ unipipython.py
@@ -2,7 +2,7 @@
 #	Title: unipipython.py
 #	Author: Matthijs van den Berg
 #	Date: 2018 somewhere
-#	Version 0.1 alfa NEEDS A LOT OF POLISHING
+#	Version 0.1 alfa
 #	Information from https://evok.api-docs.io/1.0/rest
 
 # VAN VOORBEELD SCRIPT BOVENSTAANDE SITE
@@ -41,7 +41,7 @@ class unipython(object):
 	def set_off(self, dev, circuit):
 		url=(self.base_url + dev + "/" + circuit + "")
 		payload = {"value" : 0} # voltage toe te passen 0-10 volt NOG LEVEL VAR MAKEN
-		headers = {"source-system": "matthijs-unipi"}
+		headers = {"source-system": "unipipython"}
 		try:
 			r = requests.post(url, data=payload, headers=headers)
 		except Exception as e:
@@ -54,7 +54,7 @@ class unipython(object):
 	def set_on(self, dev, circuit):
 		url=(self.base_url + dev + "/" + circuit + "/")
 		payload = {"value" : 1} # voltage toe te passen  NOG LEVEL VAR MAKEN
-		headers = {"source-system": "matthijs-unipi"}
+		headers = {"source-system": "unipipython"}
 		try:
 			r = requests.post(url, data=payload, headers=headers)
 		except Exception as e:
@@ -65,8 +65,8 @@ class unipython(object):
 	# Set device level (http://your-ip-goes-here:8080/rest/analogoutput/{circuit}?mode=Voltage)
 	def set_level(self, circuit, level):
 		url=(self.base_url + "analogoutput/" + circuit + "?mode=Voltage")
-		payload = {"value" : level} # voltage toe te passen 0-10 volt NOG LEVEL VAR MAKEN
-		headers = {"source-system": "matthijs-unipi"}
+		payload = {"value" : level} 
+		headers = {"source-system": "unipipython"}
 		try:
 			r = requests.post(url, data=payload, headers=headers)
 		except Exception as e:
@@ -78,27 +78,68 @@ class unipython(object):
 	# Get device information from Unipi and return to calling function. Json format.
 	def get_circuit(self, dev, circuit):
 		url=(self.base_url + dev + "/" + circuit + "/")
-		headers = {"source-system": "matthijs-unipi"}
+		headers = {"source-system": "unipipython"}
 		r = requests.get(url, headers=headers)
 		if(r.status_code == 200):
 			return(r.json())
 		else:
 			return('ERROR: statuscode: ' + r.status_code)
+
+	# Toggle a device (when on, off, etc.)
+	def toggle_switch(self, dev, circuit): #source_dev is switch, dev / circuit is to be switched
+		#print('toggle_switch dev started')
+		url=(self.base_url + dev + "/" + circuit + "/")
+		#print(url)
+		headers = {"source-system": "unipipython"}
+		r = requests.get(url, headers=headers)
+		#print(r)
+		if(r.status_code == 200):
+			#return(r.json())
+			status=r.json()
+			if(status['value'] == 0):
+				unipython.set_on(self, dev, circuit)
+				return(0,r.status_code)
+			elif(status['value'] == 1):
+				unipython.set_off(self, dev, circuit)
+				return(1,r.status_code)
+			else:
+				return(667,r.status_code)#print('Geen matchende status gevonden van 0 of 1')
+		else:
+			return(666,r.status_code)
+
+	# Toggle a dimm device (when on, off, etc.)
+	def toggle_dimmer(self, dev, circuit, level): #source_dev is switch, dev / circuit is to be switched
+		url=(self.base_url + dev + "/" + circuit + "/")
+		headers = {"source-system": "unipipython"}
+		r = requests.get(url, headers=headers)
+		if(r.status_code == 200):
+			status=r.json()
+			if(status['value'] == 0):
+				unipython.set_level(self, circuit, level)
+				return(1,r.status_code)
+			elif(status['value'] > 0):
+				unipython.set_level(self, circuit, 0)
+				return(0,r.status_code)
+			else:
+				return(667,r.status_code)#print('Geen matchende status gevonden van 0 of 1')
+		else:
+			return(666,r.status_code)
 		
-	#moet ik dit niet in client mqtt script oplossen?
+	#Since the bel NEEDS to be turned on and off to ring, we script on and off here. 
 	def ring_bel(self, times, dev, circuit):
 		ctr = 0
 		while (ctr < times):
 			url=(self.base_url + dev + "/" + circuit + "/")
-			print(url)
+			#print(url)
 			payload = {"value" : 1} # voltage toe te passen 0-10 volt NOG LEVEL VAR MAKEN
-			headers = {"source-system": "matthijs-unipi"}
+			headers = {"source-system": "unipipython"}
 			try:
 				result = requests.post(url, data=payload, headers=headers)
 			except Exception as e:
 				ErrorHandling(e)
 			else:
 				response = requests.post(url, data=payload, headers=headers)
+			time.sleep(0.1)
 			payload = {"value" : 0} 
 			try:
 				result = requests.post(url, data=payload, headers=headers)
@@ -106,7 +147,7 @@ class unipython(object):
 				ErrorHandling(e)
 			else:
 				response = requests.post(url, data=payload, headers=headers)
-			time.sleep(0.2)
+			time.sleep(0.3)
 			ctr += 1
-		print(response)
+		#print(response)
 		return (response) #assuming that last responce is representative for all s-:
